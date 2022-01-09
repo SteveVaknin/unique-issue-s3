1778c015c1ef66306a9a82e700fe859d16c26ad5
Steve Vaknin <62605534+SteveVaknin@users.noreply.github.com>
2022-01-09 12:48:21 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -20,6 +20,7 @@ resource "aws_s3_bucket_object" "data_object" {
     Name        = "${local.resource_prefix.value}-customer-master"
     Environment = local.resource_prefix.value
   }
+  
 }
 
 resource "aws_s3_bucket" "financials" {
